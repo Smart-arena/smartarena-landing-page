a85632071130691bd8c6ac86412e29216c5fde4d
Nnaemeka Bethrand <bethrand2019@gmail.com>
2024-01-22 16:00:58 +0100

    edits

--- app/page.tsx
+++ app/page.tsx
@@ -538,7 +538,7 @@ const Home = () => {
             Details` for more information.
           </h1>
           <Link
-            href={"/"}
+            href={"https://shop.mysmartarena.com/allproducts"}
             className="bg-dark-blue text-white px-4 py-3 rounded-lg hover:scale-110 active:scale-100 duration-200 lg:block hidden "
           >
             View all Products
